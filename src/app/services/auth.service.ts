15f7bb5d07f7806e063be1c988db18701ecd5036
redgaryesid <r.edgaryesid@gmail.com>
2025-02-14 23:35:21 -0500

    Clase 1 y 2, inicio del proyecto y flujo del login

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '@environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  apiURL = environment.API_URL;
+
+  constructor(
+    private http: HttpClient
+  ) {
+
+   }
+
+   login(email: string, password: string) {
+    // This is where the login logic would go
+    return this.http.post(`${this.apiURL}/auth/login`, {
+      email,
+      password
+    });
+
+   }
+}
